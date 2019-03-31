bd2e301c879531a1690efe26d02b49db30234423
Sam Taub <staub91@gmail.com>
2019-03-31 11:35:52 -0400

    set up directory with create react app.  added bootstrap cdn

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>South Park Click Game</h1>
+      </div>
+    )
+  }
+}
+
+export default App;
