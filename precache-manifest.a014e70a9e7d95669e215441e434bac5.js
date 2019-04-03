03e139d2ce3ad0f7023501a7f30f6c8fd891025e
Sam Taub <staub91@gmail.com>
2019-04-03 17:48:37 -0400

    Updates

---
+++ precache-manifest.a014e70a9e7d95669e215441e434bac5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a3b74089770802ae2f4d",
+    "url": "/react-clicky-game/static/js/runtime~main.a57faf00.js"
+  },
+  {
+    "revision": "2b3fcf97a948e4813e51",
+    "url": "/react-clicky-game/static/js/main.c7dbb32a.chunk.js"
+  },
+  {
+    "revision": "58f496bc1ce27350f0eb",
+    "url": "/react-clicky-game/static/js/2.98dcc9dd.chunk.js"
+  },
+  {
+    "revision": "2b3fcf97a948e4813e51",
+    "url": "/react-clicky-game/static/css/main.cd6460c4.chunk.css"
+  },
+  {
+    "revision": "c8e6622ca403eac78afa7810bec87c3e",
+    "url": "/react-clicky-game/index.html"
+  }
+];
