53fc9f187efaf5538342d97f4eb41cac029cf272
Sam Taub <staub91@gmail.com>
2019-04-03 14:15:17 -0400

    added homepage to package.json

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -8,10 +8,10 @@ class Navbar extends Component {
     render(){        
         return (
         <ul className = 'navList'>
-            <li className = "title">South Park Click Game</li>
+            <li className = "title"><img id= "signImage" alt="Sign" src="http://icons.iconarchive.com/icons/xtudiando/south-park/48/South-Park-icon.png"/>Click Game</li>
             <li className = "status">{this.props.status}</li>
+            <li className = "highScore">High Score: {this.props.highScore}</li>
             <li className = "currentScore">Current Score: {this.props.currentScore}</li>
-            <li classname = "highScore">High Score: {this.props.highScore}</li>
         </ul>
         );
     }
